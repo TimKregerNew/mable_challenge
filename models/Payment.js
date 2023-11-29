127fcaff8721377866e1409c1684ddc722fa732e
Tim Kreger <TimKreger@gmail.com>
2023-11-29 16:14:07 +1100

    Added comments is Payments and Database

--- models/Payment.js
+++ models/Payment.js
@@ -1,6 +1,9 @@
 const { Transaction, TransactionStatus } = require("./Transaction")
 const { UserAccount } = require("./UserAccount")
 
+// Is a skeleton  class to help test logic
+// Ideally it would perform the payment at the bank asynchrounously and only update once the transaction status' are confirmed
+// My thinking was to use a message queue to the payment service and wait for a return message once transaction is complete
 class Payment {
    
     processPayment(transaction) {
